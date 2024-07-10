4f1c658467a05299a76512e7b7e5ac69c96c668f
Manuel Quinto Sabelli <114246385+Manuel-QS@users.noreply.github.com>
2024-07-10 14:15:01 +0200

    Add files via upload

---
+++ UCSanDiegoX DSE230x Big Data Analytics Using Spark/SPARK - Covariance_PCA.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@author: ManuelQuintoSabelli
+April 2023
+UCSanDiegoX DSE230x
+Big Data Analytics Using Spark
+"""
+
+"""
+# SPARK - Exercises introduction
+"""
+
+#-----------------------------------------------------------------------------
